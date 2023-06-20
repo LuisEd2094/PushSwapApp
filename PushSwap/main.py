c03b3484d644db64eb0511212d97b288ba6802e9
Luis <luissoto2094@gmail.com>
2023-06-20 11:42:24 +0200

    PushSwap should work now with json files

--- PushSwap/main.py
+++ PushSwap/main.py
@@ -20,20 +20,11 @@ def get_indexed_a(stack_a):
         index_a[index] = final_pos 
     return (index_a)
 
-def get_stack_a(file):
+def get_stack_a(numbers):
     
     #should first use merge sort? to order the OG array. Then use BTree to get index for each value. It's a bit faster than checking the full array updating the POS 
-    stack_a = list()
-
-    
-    text_file = open(file, "r")
-    
-    for line in text_file:
-        line = line.strip('\n')
-        stack_a.append(int(line))
-        
+    stack_a = [int (x) for x in numbers]
     index_a = get_indexed_a(stack_a)
-
     return (index_a)
 
 def print_logic(movements):
@@ -73,157 +64,44 @@ def randomGen(n):
     return newList
          
 def main(numbers):
-    print (numbers)
-    return (numbers)
     values= []
     maxScore = 0
     maxList = list()
     failedSorts = list()
-    
-    if sys.argv[1] == "random" or sys.argv[1] == "r":
-        max_count = 1
-        max_500 = 1
-        minStac = list()
-        for argument in range(int(sys.argv[2])):
-            space = 2
+    space = 0
+    numDiv = 2
+
+    stack_a = get_stack_a(numbers)
+    copy_a = stack_a.copy()
+
+    movements = list()
+    if len(stack_a) == 3:
+        stack_a = size3.solve_for_3(stack_a, movements)
+
+    elif len(stack_a) == 5:
+        stack_a = size5.solve_for_5(stack_a, movements)
+    elif len(stack_a)>= 100:
+        #print(", ".join(['"{}"'.format(str(x)) for x in stack_a]))
+        stack_a = for100.solve_for_100(stack_a,movements, space, numDiv)
+        print_logic(movements)
+        minLen = len(movements)
+        minMovements = movements.copy()
+
+        while space < 5:
+            space += 1
             numDiv = 0
-            movements = list()
-            stack_a = randomGen(int(sys.argv[3]))
-
-            stack_a = get_indexed_a(stack_a)
-            copy_a = stack_a.copy()
-            if len(stack_a) == 5:
-                stack_a = size5.solve_for_5(stack_a, movements)
-                if not sorted(stack_a):
-                    failedSorts.append(copy_a)
-                else:
-                    values.append(len(movements))
-                if len(movements) > 12:
-                    if copy_a not in maxList:
-                        maxList.append(copy_a)
-            else:
-
-                stack_a = for100.solve_for_100(stack_a,movements, space, numDiv)
+            while numDiv < 5:
+                numDiv +=  1
+                movements = list()
+                stack_a = copy_a.copy()
+                stack_a = for100.solve_for_100(stack_a, movements, space, numDiv)
                 print_logic(movements)
-                minLen = len(movements)
-                minMovements = movements.copy()
-                while space < 4:
-                    space += 1
-                    movements = list()
-                    stack_a = copy_a.copy()
-                    stack_a = for100.solve_for_100(stack_a, movements, space, numDiv)
-                    print_logic(movements)
-                    if len(movements) < minLen:
-                        minLen = len(movements)
-                        minMovements = movements.copy()
-                    if (sorted(stack_a) != stack_a):
-                        print (f"List number {argument} not sorted sorted")
-                        failedSorts.append(copy_a)
-                values.append(minLen)
-                if len(movements) >= 700 and len(stack_a) == 100:
-                    path = r'/home/luised2094/pushswap/savedfiles/maxList_' + str(max_count)
-                    with open(path, 'w') as fp:
-                        for num in copy_a:
-                            fp.write("%i\n" % num)
-                    max_count += 1
-                elif len(movements) >= 5500 and len(stack_a) == 500:
-                    path = r'/home/luised2094/pushswap/savedfiles/maxList500_' + str(max_500)
-                    with open(path, 'w') as fp:
-                        for num in copy_a:
-                            fp.write("%i\n" % num)
-                    max_count += 1
-            
-
-                if maxList:
-                    path = r'/home/luised2094/pushswap/savedfiles/maxList'
-                    with open(path, 'w') as fp:
-                        for num in maxList:
-                            fp.write("%i\n" % num)
-                if failedSorts:
-                    path = r'/home/luised2094/pushswap/savedfiles/failed_sort'
-                    with open(path, 'w') as fp:
-                        for lists in failedSorts:
-                            for num in lists:
-                                fp.write("%i\n" % num)
-                            fp.write("Next List: \n")
-                    
-        avr = sum(values) / len(values)
-        if (failedSorts):
-            print(failedSorts)
-        if (maxList):
-            print(maxList)
-        print ('average  ' + str(avr))
-        print ('max ' + str(max(values)))
-        print ('min ' + str(min(values)))
-    
-    elif sys.argv[1] == "p":
-        for index, argument in enumerate(sys.argv):
-            space = 2
-            numDiv = 0
-            if index  < 2:
-                continue
-            stack_a = get_stack_a(argument)
-            copy_a = stack_a.copy()
-            sorted_a = sorted(stack_a)
-            movements = list()
-            if len(stack_a) == 3:
-                stack_a = size3.solve_for_3(stack_a, movements)
-            elif len(stack_a) == 5:
-                stack_a = size5.solve_for_5(stack_a, movements)
-            elif len(stack_a)>= 100:
-                stack_a = for100.solve_for_100(stack_a,movements, space, numDiv)
-                print_logic(movements)
-                minLen = len(movements)
-                minMovements = movements.copy()
-                while space < 4:
-                    space += 1
-                    movements = list()
-                    stack_a = copy_a.copy()
-                    stack_a = for100.solve_for_100(stack_a, movements, space, numDiv)
-                    print_logic(movements)
-                    if len(movements) < minLen:
-                        minLen = len(movements)
-                        minMovements = movements.copy()
+                if len(movements) < minLen:
+                    minLen = len(movements)
+                    minMovements = movements.copy()
     else:
-        for index, argument in enumerate(sys.argv):
-            space = 2
-            numDiv = 0
-            if index == 0:
-                continue
-            stack_a = get_stack_a(argument)
-            copy_a = stack_a.copy()
-
-            movements = list()
-            if len(stack_a) == 3:
-                stack_a = size3.solve_for_3(stack_a, movements)
-            elif len(stack_a) == 5:
-                stack_a = size5.solve_for_5(stack_a, movements)
-            elif len(stack_a)>= 100:
-                #print(", ".join(['"{}"'.format(str(x)) for x in stack_a]))
-                stack_a = for100.solve_for_100(stack_a,movements, space, numDiv)
-                print_logic(movements)
-                print(len(movements))
-                print(*movements, sep="\n")
-
-                minLen = len(movements)
-                minMovements = movements.copy()
-                while space < 5:
-                    space += 1
-                    numDiv = 0
-                    while numDiv < 5:
-                        numDiv +=  1
-                        movements = list()
-                        stack_a = copy_a.copy()
-                        stack_a = for100.solve_for_100(stack_a, movements, space, numDiv)
-                        print_logic(movements)
-                        if len(movements) < minLen:
-                            minLen = len(movements)
-                            minMovements = movements.copy()
-            else:
-                stack_a = morethan_5.more_than5(stack_a, movements)
-        print(stack_a)
-        print(len(minMovements))
-        return(movements)
+        stack_a = morethan_5.more_than5(stack_a, movements)
+    return (minMovements)
 
 
 def receive_json():
@@ -231,7 +109,7 @@ def receive_json():
         with open('input.json') as f:
             data = json.load(f)
         
-        message = data.get('numbers')
+        message = data.get('numbers2')
         
         result = main(message)
         print(result)
