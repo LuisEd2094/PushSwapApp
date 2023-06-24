f2ef126a893ee9d1afca68679d6168c4a9ecb7a1
Luis <luissoto2094@gmail.com>
2023-06-24 13:12:18 +0200

    started working on creating the bars

--- app/javascript/validation.js
+++ app/javascript/validation.js
@@ -10,12 +10,15 @@ document.addEventListener('turbo:load', function() {
 
             var isValid = true;
             var errorMessages = [];
+            var errorMessageContainer = document.querySelector('.error-message');
 
             
             numbersArray.forEach(function(number) {
+                if (!isValid)
+                    return;
                 var parsedNumber = parseInt(number, 10);
                 if (isNaN(parsedNumber) || number.trim() === '' || !/^[-+]?\d+$/.test(number)){
-                    errorMessages.push('Invalid Number ' + number);
+                    errorMessages.push('Please only enter numbers or a list of comma separated numbers.');
                     isValid = false;
                 }
                 else if (numbersArray.length > 1000){
@@ -23,15 +26,21 @@ document.addEventListener('turbo:load', function() {
                     isValid = false
                 }
             });
-            if (!isValid){
-                var errorMessageContainer = document.createElement('div');
-                errorMessageContainer.classList.add('error-message');
+
+            if (!isValid) {
+                if (!errorMessageContainer) {
+                  errorMessageContainer = document.createElement('div');
+                  errorMessageContainer.classList.add('error-message');
+                  numbersInput.parentElement.appendChild(errorMessageContainer);
+                }
+              
                 errorMessageContainer.innerText = errorMessages.join('\n');
-                numbersInput.parentElement.appendChild(errorMessageContainer);
-            }
-            else{
-                numberForm.submit();
-            }
+              } else {
+                if (errorMessageContainer) {
+                  errorMessageContainer.remove();
+                  numberForm.submit();
+                }
+              }
         });
     }
 })
