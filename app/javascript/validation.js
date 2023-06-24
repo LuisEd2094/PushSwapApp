492a9ab17ec522854aa9a082eb923078797b3630
Luis <luissoto2094@gmail.com>
2023-06-24 12:23:29 +0200

    still trying to get it to not submit twice

--- app/javascript/validation.js
+++ app/javascript/validation.js
@@ -1,27 +1,37 @@
-document.getElementById('number-form').addEventListener('submit', function(event) {
-    const numbersInput = document.getElementById('numbers-input');
-    const numbers = numbersInput.value.trim();
-    const numbersArray = numbers.split(/[,\s]+/);
+document.addEventListener('turbo:load', function() {
+    const numberForm = document.getElementById('number-form');
 
-    var isValid = true;
-    var errorMessages = [];
-    
-    numbersArray.forEach(function(number) {
-        var parsedNumber = parseInt(number, 10);
-        if (isNaN(parsedNumber) || number.trim() === '' || !/^[-+]?\d+$/.test(number)){
-            errorMessages.push('Invalid Number ' + number);
-            isValid = false;
-        }
-        else if (numbersArray.length > 1000){
-            errorMessages.push('Number count exceedes the limit. Maximun of 1000 are accepted.')
-            isValid = false
-        }
-    });
-    if (!isValid){
-        var errorMessageContainer = document.createElement('div');
-        errorMessageContainer.classList.add('error-message');
-        errorMessageContainer.innerText = errorMessages.join('\n');
-        numbersInput.parentElement.appendChild(errorMessageContainer);
-        return false;
+    if (numberForm) {
+        numberForm.addEventListener('turbo:submit-start', function(event) {
+            event.preventDefault();
+            const numbersInput = document.getElementById('numbers-input');
+            const numbers = numbersInput.value.trim();
+            const numbersArray = numbers.split(/[,\s]+/);
+
+            var isValid = true;
+            var errorMessages = [];
+
+            
+            numbersArray.forEach(function(number) {
+                var parsedNumber = parseInt(number, 10);
+                if (isNaN(parsedNumber) || number.trim() === '' || !/^[-+]?\d+$/.test(number)){
+                    errorMessages.push('Invalid Number ' + number);
+                    isValid = false;
+                }
+                else if (numbersArray.length > 1000){
+                    errorMessages.push('Number count exceedes the limit. Maximun of 1000 are accepted.')
+                    isValid = false
+                }
+            });
+            if (!isValid){
+                var errorMessageContainer = document.createElement('div');
+                errorMessageContainer.classList.add('error-message');
+                errorMessageContainer.innerText = errorMessages.join('\n');
+                numbersInput.parentElement.appendChild(errorMessageContainer);
+                event.preventDefault(); // Prevent form submission
+
+            }
+ 
+        });
     }
 })
