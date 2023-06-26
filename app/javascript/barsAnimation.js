bccd818cfcb37adff10972758a0c9b68e5b239d3
Luis <luissoto2094@gmail.com>
2023-06-26 14:20:27 +0200

    created stack - a and b for and properly select elements inside it for the transitions

--- app/javascript/barsAnimation.js
+++ app/javascript/barsAnimation.js
@@ -19,14 +19,15 @@ async function makeMoves(moves){
   for (let i = 0; i < moves.length; i++){
     const move = moves[i];
 
-    const transforTime = 0.1;
+    const transforTime = 0.5;
     const transitionString = 'transform ' + transforTime + 's ease-in-out'
     const promiseWait = transforTime * 1000;
 
     console.log(move);
     if (move == 'sa'){
-        var bar1 = document.querySelector('.bar-left-container:first-child');
-        var bar2 = document.querySelector('.bar-left-container:nth-child(2)');
+        var bar1 = document.getElementById('stack-a').firstChild;
+        var bar2 = document.getElementById('stack-a').children.item(1);
+
 
         await new Promise(resolve => setTimeout(resolve, promiseWait));
 
@@ -50,9 +51,9 @@ async function makeMoves(moves){
 
 
     else if (move == 'rra'){
-      var bar1 = document.querySelector('.bar-left-container:last-child');
-      var bar2 = document.querySelector('.bar-left-container:first-child');
-      const transitionElements = document.querySelectorAll('.bar-left-container:not(:first-child):not(:last-child)');
+      var bar1 = document.getElementById('stack-a').lastChild;
+      var bar2 = document.getElementById('stack-a').firstChild;
+      const transitionElements = document.querySelectorAll('#stack-a :not(:first-child):not(:last-child)');
 
       await new Promise(resolve => setTimeout(resolve, promiseWait));
       bar1.style.transition = transitionString
@@ -60,8 +61,8 @@ async function makeMoves(moves){
 
       
       bar1.style.transform = 'translateY(-200%)';
-
       bar2.style.transform = 'translateY(100%)';
+      
       transitionElements.forEach(element => {
         element.style.transition = transitionString
         element.style.transform = 'translateY(100%)'
@@ -103,8 +104,10 @@ function firstDraw(relativeValues){
 
   leftDiv.style.width = '50%';
   leftDiv.style.height = '100%';
+  leftDiv.setAttribute('id', 'stack-a');
   rightDiv.style.width = '50%';
   rightDiv.style.height = '100%';
+  rightDiv.setAttribute('id', 'stack-b');
 
   barsContainer.appendChild(leftDiv);
   barsContainer.appendChild(rightDiv);
@@ -112,7 +115,7 @@ function firstDraw(relativeValues){
 
   relativeValues.forEach(function(value) {
     var bar = document.createElement('div');
-    bar.className = 'bar-left-container';
+    bar.className = 'bar';
     var barHeightPercentage = (1 / relativeValues.length) * 100; 
     bar.style.height = barHeightPercentage + '%';
     var widthPercentage = (value / relativeValues.length) * 100;
