3ea0e8d8211bce06bec11aa9c90c02aa88468187
Luis <luissoto2094@gmail.com>
2023-06-26 15:19:01 +0200

    it handles the 3 basic movements, sa, rra, and ra. It should have the ability to handle the B variants but I need to handle PA and PB first so that I can populate B

--- app/javascript/barsAnimation.js
+++ app/javascript/barsAnimation.js
@@ -17,22 +17,22 @@ function barsAnimation(numbers, moves){
 async function makeMoves(moves){
   for (let i = 0; i < moves.length; i++){
     const move = moves[i];
-    const transforTime = 0.5;
+    const transforTime = 0.1;
     const transitionString = 'transform ' + transforTime + 's ease-in-out'
     const promiseWait = transforTime * 1000;
 
     console.log(move);
     if (move == 'sa'){
-        moveSA(transitionString, promiseWait);
+        moveSA(transitionString, promiseWait, move.slice(-1));
         await new Promise(resolve => setTimeout(resolve, promiseWait));
       }
     else if (move == 'rra'){
-      moveRRA (transitionString, promiseWait);
+      moveRRA (transitionString, promiseWait, move.slice(-1));
       await new Promise(resolve => setTimeout(resolve, promiseWait));
     }
     else if (move == 'ra')
     {
-      moveRA(transitionString, promiseWait);
+      moveRA(transitionString, promiseWait, move.slice(-1));
       await new Promise(resolve => setTimeout(resolve, promiseWait));
     }
     await new Promise(resolve => setTimeout(resolve, promiseWait ));
@@ -41,12 +41,10 @@ async function makeMoves(moves){
 
 
 
-async function moveRA(transitionString, promiseWait){
-  var lastBar = document.getElementById('stack-a').lastChild;
-  var firstBar = document.getElementById('stack-a').firstChild;
-  const translateYValue = (document.getElementById('stack-a').childElementCount - 1) * 100;
-  const transitionElements = document.querySelectorAll('#stack-a :not(:first-child):not(:last-child)');
-
+async function moveRA(transitionString, promiseWait, stack){
+  
+  const [lastBar, firstBar, translateYValue, transitionElements, parent] = assignVariables(stack);
+  
   await new Promise(resolve => setTimeout(resolve, promiseWait));
   lastBar.style.transition = transitionString
   firstBar.style.transition = transitionString
@@ -61,7 +59,6 @@ async function moveRA(transitionString, promiseWait){
   });
   await new Promise(resolve => setTimeout(resolve, promiseWait));
 
-  const parent = lastBar.parentNode;
   parent.insertBefore(firstBar, parent.lastChild.nextSibling);
   lastBar.style.transition = '';
   firstBar.style.transition = '';
@@ -78,9 +75,11 @@ async function moveRA(transitionString, promiseWait){
 }
 
 
-async function moveSA(transitionString, promiseWait){
-  var lastBar = document.getElementById('stack-a').firstChild;
-  var firstBar = document.getElementById('stack-a').children.item(1);
+async function moveSA(transitionString, promiseWait, stack){
+  var lastBar = document.getElementById('stack-' + stack).firstChild;
+  var firstBar = document.getElementById('stack-' + stack).children.item(1);
+  const parent = lastBar.parentNode;
+
 
 
   await new Promise(resolve => setTimeout(resolve, promiseWait));
@@ -94,7 +93,6 @@ async function moveSA(transitionString, promiseWait){
   await new Promise(resolve => setTimeout(resolve, promiseWait));
 
 
-  const parent = lastBar.parentNode;
   parent.insertBefore(firstBar, lastBar);
   parent.insertBefore(lastBar, firstBar.nextSibling);
   lastBar.style.transition = '';
@@ -105,13 +103,12 @@ async function moveSA(transitionString, promiseWait){
 }
 
 
-async function moveRRA(transitionString, promiseWait){
+async function moveRRA(transitionString, promiseWait, stack){
   
-  var lastBar = document.getElementById('stack-a').lastChild;
-  var firstBar = document.getElementById('stack-a').firstChild;
-  const translateYValue = (document.getElementById('stack-a').childElementCount - 1) * 100;
-  const transitionElements = document.querySelectorAll('#stack-a :not(:first-child):not(:last-child)');
+  const [lastBar, firstBar, translateYValue, transitionElements, parent] = assignVariables(stack);
 
+
+  console.log (transitionElements)
   await new Promise(resolve => setTimeout(resolve, promiseWait));
   lastBar.style.transition = transitionString
   firstBar.style.transition = transitionString
@@ -125,7 +122,6 @@ async function moveRRA(transitionString, promiseWait){
     element.style.transform = 'translateY(100%)'
   });
   await new Promise(resolve => setTimeout(resolve, promiseWait));
-  const parent = lastBar.parentNode;
   parent.insertBefore(lastBar, firstBar);
   parent.insertBefore(firstBar, lastBar.nextSibling);
   lastBar.style.transition = '';
@@ -179,4 +175,16 @@ function firstDraw(relativeValues){
     leftDiv.appendChild(bar);
   });
 
+}
+
+
+
+function assignVariables(stack) {
+  var lastBar = document.getElementById('stack-' + stack).lastChild;
+  var firstBar = document.getElementById('stack-' + stack).firstChild;
+  const translateYValue = (document.getElementById('stack-' + stack).childElementCount - 1) * 100;
+  const transitionElements = document.querySelectorAll('#stack-'+ stack + ' :not(:first-child):not(:last-child)');
+  const parent = lastBar.parentNode;
+
+  return [lastBar, firstBar, translateYValue, transitionElements, parent];
 }
